61c8953de2165d2f120e7971b9d3f24445eac23d
nissymori <kyoudai.630@gmail.com>
2024-05-02 16:12:16 +0900

    .

--- report.sh
+++ report.sh
@@ -2,30 +2,30 @@
 cd algo
 for seed in 1 2 3 4 5
 do  
-    python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=4
-    python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=8
-    python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=16
-    python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=32
-    python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=64
+    python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=4
+    python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=8
+    python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=16
+    python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=32
+    python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=64
 done
 
 # IQL
 for seed in 1 2 3 4 5
 do  
-    python iql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=4
-    python iql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=8
-    python iql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=16
-    python iql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=32
-    python iql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=64
+    python iql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=4
+    python iql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=8
+    python iql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=16
+    python iql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=32
+    python iql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=64
 done
 
 # AWAC
 for seed in 1 2 3 4 5
 do  
-    python awac.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=4
-    python awac.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=8
-    python awac.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=16
-    python awac.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=32
-    python awac.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=time n_updates=64
+    python awac.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=4
+    python awac.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=8
+    python awac.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=16
+    python awac.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=32
+    python awac.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=measure_time n_updates=64
 done
 
