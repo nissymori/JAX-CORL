2df6a21ee08b0b0a47ec29005f4fa75d931a6726
nissymori <kyoudai.630@gmail.com>
2024-07-11 04:09:43 +0900

    .

--- scripts/cql.sh
+++ scripts/cql.sh
@@ -3,10 +3,10 @@ project=cql-report
 cd .. && cd algos
 for seed in 1 2 3 4 5
 do
-    python cql.py env=halfcheetah-medium-v2 seed=$seed project=$project
-    python cql.py env=halfcheetah-medium-expert-v2 seed=$seed project=$project
-    python cql.py env=hopper-medium-v2 seed=$seed project=$project
-    python cql.py env=hopper-medium-expert-v2 seed=$seed project=$project
-    python cql.py env=walker2d-medium-v2 seed=$seed project=$project
-    python cql.py env=walker2d-medium-expert-v2 seed=$seed project=$project
+    python cql.py env_name=halfcheetah-medium-v2 seed=$seed project=$project
+    python cql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=$project
+    python cql.py env_name=hopper-medium-v2 seed=$seed project=$project
+    python cql.py env_name=hopper-medium-expert-v2 seed=$seed project=$project
+    python cql.py env_name=walker2d-medium-v2 seed=$seed project=$project
+    python cql.py env_name=walker2d-medium-expert-v2 seed=$seed project=$project
 done
--- scripts/dt.sh
+++ scripts/dt.sh
@@ -3,10 +3,10 @@ project=dt-report
 cd .. && cd algos
 for seed in 1 2 3 4 5
 do
-    python dt.py env=halfcheetah-medium-v2 seed=$seed project=$project
-    python dt.py env=halfcheetah-medium-expert-v2 seed=$seed project=$project
-    python dt.py env=hopper-medium-v2 seed=$seed project=$project
-    python dt.py env=hopper-medium-expert-v2 seed=$seed project=$project
-    python dt.py env=walker2d-medium-v2 seed=$seed project=$project
-    python dt.py env=walker2d-medium-expert-v2 seed=$seed project=$project
+    python dt.py env_name=halfcheetah-medium-v2 seed=$seed project=$project
+    python dt.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=$project
+    python dt.py env_name=hopper-medium-v2 seed=$seed project=$project
+    python dt.py env_name=hopper-medium-expert-v2 seed=$seed project=$project
+    python dt.py env_name=walker2d-medium-v2 seed=$seed project=$project
+    python dt.py env_name=walker2d-medium-expert-v2 seed=$seed project=$project
 done
