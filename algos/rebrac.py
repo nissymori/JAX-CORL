5c5013cc9923e89fca703c050bd218a7afe4c562
nissymori <kyoudai.630@gmail.com>
2024-12-01 00:54:38 +0900

    transition working

--- algos/rebrac.py
+++ algos/rebrac.py
@@ -55,14 +55,15 @@ class ReBRACConfig(BaseModel):
     env_name: str = "halfcheetah-medium-v2"
     batch_size: int = 256
     max_steps: int = 1000000
+    data_size: int = 1000000
     normalize_reward: bool = False
-    normalize_states: bool = False
+    normalize_state: bool = False
     n_jitted_updates: int = 8
     # evaluation params
     eval_episodes: int = 10
     eval_interval: int = 100000
     # general params
-    train_seed: int = 0
+    seed: int = 0
     eval_seed: int = 42
     
     def __hash__(
@@ -78,31 +79,6 @@ from copy import deepcopy
 from tqdm.auto import trange
 from typing import Sequence, Dict, Callable, Tuple, Union
 
-
-@chex.dataclass(frozen=True)
-class Metrics:
-    accumulators: Dict[str, Tuple[jax.Array, jax.Array]]
-
-    @staticmethod
-    def create(metrics: Sequence[str]) -> "Metrics":
-        init_metrics = {key: (jnp.array([0.0]), jnp.array([0.0])) for key in metrics}
-        return Metrics(accumulators=init_metrics)
-
-    def update(self, updates: Dict[str, jax.Array]) -> "Metrics":
-        new_accumulators = deepcopy(self.accumulators)
-        for key, value in updates.items():
-            acc, steps = new_accumulators[key]
-            new_accumulators[key] = (acc + value, steps + 1)
-
-        return self.replace(accumulators=new_accumulators)
-
-    def compute(self) -> Dict[str, np.ndarray]:
-        # cumulative_value / total_steps
-        return {k: np.array(v[0] / v[1]) for k, v in self.accumulators.items()}
-
-
-def normalize(arr: jax.Array, mean: jax.Array, std: jax.Array, eps: float = 1e-8) -> jax.Array:
-    return (arr - mean) / (std + eps)
 
 
 def evaluate(env: gym.Env, params, action_fn: Callable, num_episodes: int, seed: int) -> np.ndarray:
@@ -122,143 +98,6 @@ def evaluate(env: gym.Env, params, action_fn: Callable, num_episodes: int, seed:
         returns.append(total_reward)
 
     return np.array(returns)
-
-
-def qlearning_dataset(env, dataset=None, terminate_on_end=False, **kwargs):
-    """
-    Returns datasets formatted for use by standard Q-learning algorithms,
-    with observations, actions, next_observations, next_actins, rewards,
-     and a terminal flag.
-    Args:
-        env: An OfflineEnv object.
-        dataset: An optional dataset to pass in for processing. If None,
-            the dataset will default to env.get_dataset()
-        terminate_on_end (bool): Set done=True on the last timestep
-            in a trajectory. Default is False, and will discard the
-            last timestep in each trajectory.
-        **kwargs: Arguments to pass to env.get_dataset().
-    Returns:
-        A dictionary containing keys:
-            observations: An N x dim_obs array of observations.
-            actions: An N x dim_action array of actions.
-            next_observations: An N x dim_obs array of next observations.
-            next_actions: An N x dim_action array of next actions.
-            rewards: An N-dim float array of rewards.
-            terminals: An N-dim boolean array of "done" or episode termination flags.
-    """
-    if dataset is None:
-        dataset = env.get_dataset(**kwargs)
-
-    N = dataset['rewards'].shape[0]
-    obs_ = []
-    next_obs_ = []
-    action_ = []
-    next_action_ = []
-    reward_ = []
-    done_ = []
-
-    # The newer version of the dataset adds an explicit
-    # timeouts field. Keep old method for backwards compatability.
-    use_timeouts = 'timeouts' in dataset
-
-    episode_step = 0
-    for i in range(N - 1):
-        obs = dataset['observations'][i].astype(np.float32)
-        new_obs = dataset['observations'][i + 1].astype(np.float32)
-        action = dataset['actions'][i].astype(np.float32)
-        new_action = dataset['actions'][i + 1].astype(np.float32)
-        reward = dataset['rewards'][i].astype(np.float32)
-        done_bool = bool(dataset['terminals'][i])
-
-        if use_timeouts:
-            final_timestep = dataset['timeouts'][i]
-        else:
-            final_timestep = (episode_step == env._max_episode_steps - 1)
-        if (not terminate_on_end) and final_timestep:
-            # Skip this transition and don't apply terminals on the last step of an episode
-            episode_step = 0
-            continue
-        if done_bool or final_timestep:
-            episode_step = 0
-
-        obs_.append(obs)
-        next_obs_.append(new_obs)
-        action_.append(action)
-        next_action_.append(new_action)
-        reward_.append(reward)
-        done_.append(done_bool)
-        episode_step += 1
-
-    return {
-        'observations': np.array(obs_),
-        'actions': np.array(action_),
-        'next_observations': np.array(next_obs_),
-        'next_actions': np.array(next_action_),
-        'rewards': np.array(reward_),
-        'terminals': np.array(done_),
-    }
-
-
-def compute_mean_std(states: jax.Array, eps: float) -> Tuple[jax.Array, jax.Array]:
-    mean = states.mean(0)
-    std = states.std(0) + eps
-    return mean, std
-
-
-def normalize_states(states: jax.Array, mean: jax.Array, std: jax.Array):
-    return (states - mean) / std
-
-
-@chex.dataclass
-class ReplayBuffer:
-    data: Dict[str, jax.Array] = None
-    mean: float = 0
-    std: float = 1
-
-    def create_from_d4rl(self, env_name: str, normalize_reward: bool = False,
-                         normalize: bool = False):
-        d4rl_data = qlearning_dataset(gym.make(env_name))
-        buffer = {
-            "states": jnp.asarray(d4rl_data["observations"], dtype=jnp.float32),
-            "actions": jnp.asarray(d4rl_data["actions"], dtype=jnp.float32),
-            "rewards": jnp.asarray(d4rl_data["rewards"], dtype=jnp.float32),
-            "next_states": jnp.asarray(d4rl_data["next_observations"], dtype=jnp.float32),
-            "next_actions": jnp.asarray(d4rl_data["next_actions"], dtype=jnp.float32),
-            "dones": jnp.asarray(d4rl_data["terminals"], dtype=jnp.float32)
-        }
-        if normalize:
-            self.mean, self.std = compute_mean_std(buffer["states"], eps=1e-3)
-            buffer["states"] = normalize_states(
-                buffer["states"], self.mean, self.std
-            )
-            buffer["next_states"] = normalize_states(
-                buffer["next_states"], self.mean, self.std
-            )
-        if normalize_reward:
-            buffer["rewards"] = ReplayBuffer.normalize_reward(env_name, buffer["rewards"])
-        self.data = buffer
-
-    @property
-    def size(self):
-        # WARN: do not use __len__ here! It will use len of the dataclass, i.e. number of fields.
-        return self.data["states"].shape[0]
-
-    def sample_batch(self, key: jax.random.PRNGKey, batch_size: int) -> Dict[str, jax.Array]:
-        indices = jax.random.randint(key, shape=(batch_size,), minval=0, maxval=self.size)
-        batch = jax.tree_map(lambda arr: arr[indices], self.data)
-        return batch
-
-    def get_moments(self, modality: str) -> Tuple[jax.Array, jax.Array]:
-        mean = self.data[modality].mean(0)
-        std = self.data[modality].std(0)
-        return mean, std
-
-    @staticmethod
-    def normalize_reward(env_name: str, rewards: jax.Array) -> jax.Array:
-        if "antmaze" in env_name:
-            return rewards * 100.0  # like in LAPO
-        else:
-            raise NotImplementedError("Reward normalization is implemented only for AntMaze yet!")
 
 
 default_kernel_init = nn.initializers.lecun_normal()
@@ -375,6 +214,135 @@ class EnsembleCritic(nn.Module):
         return q_values
 
 
+class Transition(NamedTuple):
+    observations: jnp.ndarray
+    actions: jnp.ndarray
+    rewards: jnp.ndarray
+    next_observations: jnp.ndarray
+    next_actions: jnp.ndarray
+    dones: jnp.ndarray
+
+
+def qlearning_dataset(env, dataset=None, terminate_on_end=False, **kwargs):
+    """
+    Returns datasets formatted for use by standard Q-learning algorithms,
+    with observations, actions, next_observations, next_actins, rewards,
+     and a terminal flag.
+    Args:
+        env: An OfflineEnv object.
+        dataset: An optional dataset to pass in for processing. If None,
+            the dataset will default to env.get_dataset()
+        terminate_on_end (bool): Set done=True on the last timestep
+            in a trajectory. Default is False, and will discard the
+            last timestep in each trajectory.
+        **kwargs: Arguments to pass to env.get_dataset().
+    Returns:
+        A dictionary containing keys:
+            observations: An N x dim_obs array of observations.
+            actions: An N x dim_action array of actions.
+            next_observations: An N x dim_obs array of next observations.
+            next_actions: An N x dim_action array of next actions.
+            rewards: An N-dim float array of rewards.
+            terminals: An N-dim boolean array of "done" or episode termination flags.
+    """
+    if dataset is None:
+        dataset = env.get_dataset(**kwargs)
+
+    N = dataset['rewards'].shape[0]
+    obs_ = []
+    next_obs_ = []
+    action_ = []
+    next_action_ = []
+    reward_ = []
+    done_ = []
+
+    # The newer version of the dataset adds an explicit
+    # timeouts field. Keep old method for backwards compatability.
+    use_timeouts = 'timeouts' in dataset
+
+    episode_step = 0
+    for i in range(N - 1):
+        obs = dataset['observations'][i].astype(np.float32)
+        new_obs = dataset['observations'][i + 1].astype(np.float32)
+        action = dataset['actions'][i].astype(np.float32)
+        new_action = dataset['actions'][i + 1].astype(np.float32)
+        reward = dataset['rewards'][i].astype(np.float32)
+        done_bool = bool(dataset['terminals'][i])
+
+        if use_timeouts:
+            final_timestep = dataset['timeouts'][i]
+        else:
+            final_timestep = (episode_step == env._max_episode_steps - 1)
+        if (not terminate_on_end) and final_timestep:
+            # Skip this transition and don't apply terminals on the last step of an episode
+            episode_step = 0
+            continue
+        if done_bool or final_timestep:
+            episode_step = 0
+
+        obs_.append(obs)
+        next_obs_.append(new_obs)
+        action_.append(action)
+        next_action_.append(new_action)
+        reward_.append(reward)
+        done_.append(done_bool)
+        episode_step += 1
+
+    return {
+        'observations': np.array(obs_),
+        'actions': np.array(action_),
+        'next_observations': np.array(next_obs_),
+        'next_actions': np.array(next_action_),
+        'rewards': np.array(reward_),
+        'terminals': np.array(done_),
+    }
+
+
+def get_dataset(
+    env: gym.Env, config: ReBRACConfig, clip_to_eps: bool = True, eps: float = 1e-5
+) -> Transition:
+    dataset = qlearning_dataset(env)
+
+    if clip_to_eps:
+        lim = 1 - eps
+        dataset["actions"] = np.clip(dataset["actions"], -lim, lim)
+
+    imputed_next_observations = np.roll(dataset["observations"], -1, axis=0)
+    same_obs = np.all(
+        np.isclose(imputed_next_observations, dataset["next_observations"], atol=1e-5),
+        axis=-1,
+    )
+    dones = 1.0 - same_obs.astype(np.float32)
+    dones[-1] = 1
+
+    dataset = Transition(
+        observations=jnp.array(dataset["observations"], dtype=jnp.float32),
+        actions=jnp.array(dataset["actions"], dtype=jnp.float32),
+        rewards=jnp.array(dataset["rewards"], dtype=jnp.float32),
+        dones=jnp.array(dones, dtype=jnp.float32),
+        next_observations=jnp.array(dataset["next_observations"], dtype=jnp.float32),
+        next_actions=jnp.array(dataset["next_actions"], dtype=jnp.float32),
+    )
+    # shuffle data and select the first data_size samples
+    data_size = min(config.data_size, len(dataset.observations))
+    rng = jax.random.PRNGKey(config.seed)
+    rng, rng_permute, rng_select = jax.random.split(rng, 3)
+    perm = jax.random.permutation(rng_permute, len(dataset.observations))
+    dataset = jax.tree_util.tree_map(lambda x: x[perm], dataset)
+    assert len(dataset.observations) >= data_size
+    dataset = jax.tree_util.tree_map(lambda x: x[:data_size], dataset)
+    # normalize states
+    obs_mean, obs_std = 0, 1
+    if config.normalize_state:
+        obs_mean = dataset.observations.mean(0)
+        obs_std = dataset.observations.std(0)
+        dataset = dataset._replace(
+            observations=(dataset.observations - obs_mean) / (obs_std + 1e-5),
+            next_observations=(dataset.next_observations - obs_mean) / (obs_std + 1e-5),
+        )
+    return dataset, obs_mean, obs_std
+
+
 class CriticTrainState(TrainState):
     target_params: FrozenDict
 
@@ -399,10 +367,10 @@ class ReBRAC(object):
         key, random_action_key = jax.random.split(rng, 2)
 
         def actor_loss_fn(params):
-            actions = train_state.actor.apply_fn(params, batch["states"])
+            actions = train_state.actor.apply_fn(params, batch.observations)
 
-            bc_penalty = ((actions - batch["actions"]) ** 2).sum(-1)
-            q_values = train_state.critic.apply_fn(train_state.critic.params, batch["states"], actions).min(0)
+            bc_penalty = ((actions - batch.actions) ** 2).sum(-1)
+            q_values = train_state.critic.apply_fn(train_state.critic.params, batch.observations, actions).min(0)
             lmbda = 1
             if config.normalize_q:
                 lmbda = jax.lax.stop_gradient(1 / jax.numpy.abs(q_values).mean())
@@ -410,7 +378,7 @@ class ReBRAC(object):
             loss = (config.actor_bc_coef * bc_penalty - lmbda * q_values).mean()
 
             # logging stuff
-            random_actions = jax.random.uniform(random_action_key, shape=batch["actions"].shape, minval=-1.0, maxval=1.0)
+            random_actions = jax.random.uniform(random_action_key, shape=batch.actions.shape, minval=-1.0, maxval=1.0)
             return loss
 
         actor_loss, grads = jax.value_and_grad(actor_loss_fn)(train_state.actor.params)
@@ -435,22 +403,22 @@ class ReBRAC(object):
     ) -> Tuple[SACNState, jax.Array]:
         key, actions_key = jax.random.split(rng)
 
-        next_actions = train_state.actor.apply_fn(train_state.actor.target_params, batch["next_states"])
+        next_actions = train_state.actor.apply_fn(train_state.actor.target_params, batch.next_observations)
         noise = jax.numpy.clip(
             (jax.random.normal(actions_key, next_actions.shape) * config.policy_noise),
             -config.noise_clip,
             config.noise_clip,
         )
         next_actions = jax.numpy.clip(next_actions + noise, -1, 1)
-        bc_penalty = ((next_actions - batch["next_actions"]) ** 2).sum(-1)
-        next_q = train_state.critic.apply_fn(train_state.critic.target_params, batch["next_states"], next_actions).min(0)
+        bc_penalty = ((next_actions - batch.next_actions) ** 2).sum(-1)
+        next_q = train_state.critic.apply_fn(train_state.critic.target_params, batch.next_observations, next_actions).min(0)
         next_q = next_q - config.critic_bc_coef * bc_penalty
 
-        target_q = batch["rewards"] + (1 - batch["dones"]) * config.gamma * next_q
+        target_q = batch.rewards + (1 - batch.dones) * config.gamma * next_q
 
         def critic_loss_fn(critic_params):
             # [N, batch_size] - [1, batch_size]
-            q = train_state.critic.apply_fn(critic_params, batch["states"], batch["actions"])
+            q = train_state.critic.apply_fn(critic_params, batch.observations, batch.actions)
             loss = ((q - target_q[None, ...]) ** 2).mean(1).sum(0)
             return loss
 
@@ -462,13 +430,14 @@ class ReBRAC(object):
     def update_n_times(
         self,
         train_state: SACNState,
-        buffer: ReplayBuffer,
+        dataset: Transition,
         rng: jax.random.PRNGKey,
         config: ReBRACConfig,
     ):
         for _ in range(config.n_jitted_updates):
             rng, batch_rng, critic_rng, actor_rng = jax.random.split(rng, 4)
-            batch = buffer.sample_batch(batch_rng, config.batch_size)
+            indices = jax.random.randint(batch_rng, shape=(config.batch_size,), minval=0, maxval=len(dataset.observations))
+            batch = jax.tree_util.tree_map(lambda x: x[indices], dataset)
 
             train_state, critic_loss = self.update_critic(train_state, batch, critic_rng, config)
             if _ % config.policy_freq == 0:
@@ -487,7 +456,7 @@ def action_fn(actor: TrainState) -> Callable:
 
 
 def create_train_state(observation, action, config):
-    key = jax.random.PRNGKey(seed=config.train_seed)
+    key = jax.random.PRNGKey(seed=config.seed)
     key, actor_key, critic_key = jax.random.split(key, 3)
 
     action_dim = action.shape[-1]
@@ -523,12 +492,11 @@ if __name__ == "__main__":
         id=str(uuid.uuid4()),
     )
     wandb.mark_preempting()
-    buffer = ReplayBuffer()
-    buffer.create_from_d4rl(config.env_name, config.normalize_reward, config.normalize_states)
     env = gym.make(config.env_name)
-
-    key = jax.random.PRNGKey(seed=config.train_seed)
-    train_state = create_train_state(buffer.data["states"][0], buffer.data["actions"][0], config)
+    dataset, obs_mean, obs_std = get_dataset(env, config)
+    key = jax.random.PRNGKey(seed=config.seed)
+    example_batch = jax.tree_util.tree_map(lambda x: x[0], dataset)
+    train_state = create_train_state(example_batch.observations, example_batch.actions, config)
 
     rebrac = ReBRAC()
     
@@ -541,7 +509,7 @@ if __name__ == "__main__":
     eval_interval = int(config.eval_interval / config.n_jitted_updates)
     for step in trange(num_steps, desc="ReBRAC Steps"):
         key, subkey = jax.random.split(key)
-        train_state, info = rebrac.update_n_times(train_state, buffer, subkey, config)
+        train_state, info = rebrac.update_n_times(train_state, dataset, subkey, config)
         wandb.log({"epoch": step, **{f"ReBRAC/{k}": v for k, v in info.items()}})
 
         if step % eval_interval == 0 or step == num_steps - 1:
