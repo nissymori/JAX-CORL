7f8346f508127683408609a5c8fa1dd1b077636f
Soichiro Nishimori <55940613+nissymori@users.noreply.github.com>
2024-12-17 17:30:07 +0900

    remove files (#112)

--- scripts/cql.sh
+++ scripts/cql.sh
@@ -1,7 +1,7 @@
-project=cql-report
+project=cql-report-separate
 
 cd .. && cd algos
-for seed in 1 2 3 4 5
+for seed in 1 2 3 4 5 6 7 8 9 10
 do
     python cql.py env_name=halfcheetah-medium-v2 seed=$seed project=$project
     python cql.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=$project
--- scripts/td3bc.sh
+++ scripts/td3bc.sh
@@ -1,7 +1,7 @@
-project=td3bc-report
+project=td3bc-report-separate
 
 cd .. && cd algos
-for seed in 1 2 3 4 5
+for seed in 1 2 3 4 5 6 7 8 9 10
 do
     python td3bc.py env_name=halfcheetah-medium-v2 seed=$seed project=$project
     python td3bc.py env_name=halfcheetah-medium-expert-v2 seed=$seed project=$project
